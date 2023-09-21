f737c80d8a466c5a5b254e352c6ac1864a1b9dd3
kavikkannan <kavikkannan.k@gmail.com>
2023-09-21 19:36:20 +0530

    kk

--- src/app/Components/QuestionWheel.jsx
+++ src/app/Components/QuestionWheel.jsx
@@ -7,60 +7,132 @@ function QuestionWheel() {
     let isTrueQ1 = false;
     
     // let isTrueQ2 = true;
-    // let .... 
+    // let zdhasharw.... 
 
     return (
         <div className="bg-opacity-10 bg-gradient-to-b from-[#4d4f4f6a] from-10% via-[#42434462] via-30% to-[#41414100] to-100% ... text-white max-h-[100%] max-w-[100%] overflow-y-auto grid grid-rows-auto gap-0 z-0 m-2">
             <ul className={"md:flex-auto px-4 z-0 py-3"}>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center  `}>
 
-                    Quesiton 1
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
 
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 2
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 3
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 4
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 5
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 6
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 7
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 8
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 9
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
-                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center bg-${isTrueQ1 ? 'green-500' : 'red-300'} ${isTrueQ1 ? 'bg-opacity-60' : 'bg-opacity-20'} animate-glow-opacity-${isTrueQ1 ? 'green' : 'red'} hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100`}>
-                    <Link href="/">
-                        Question 10
-                    </Link>
+                <li className={`px-7 py-3 mb-4 mt-3 transition duration-300 ease-in-out opacity-80 transform text-center `}>
+                <div className="mb-2">
+                    <p className="text-white text-xl font-bold">Question 1</p>
+                    </div>
+                    <div className="bg-01071D p-3 rounded-md text-left">
+                        <span className='mb-4'>Textbox content for Question 1</span>
+                        <textarea
+                            className="w-full h-16 bg-transparent text-white border-none focus:outline-none mt-10 bg-01071D bg-opacity-72"
+                            placeholder="Enter your answer here..."
+                        ></textarea>
+                    </div>
                 </li>
             </ul>
 
